7ad783c772cc8b4f0aec601c7191b80a05aa7b6e
applepiesss <nadiaaiiisyahf@gmail.com>
2026-04-06 00:50:22 +0700

    [RED] Added JPA repo for foreman view

--- src/main/java/com/mysawit/harvest/repository/HarvestRepository.java
+++ src/main/java/com/mysawit/harvest/repository/HarvestRepository.java
@@ -8,7 +8,16 @@ import java.util.List;
 import java.util.UUID;
 
 public interface HarvestRepository extends JpaRepository<Harvest, UUID> {
+    // Harvester log
     boolean existsHarvestByHarvesterIdAndHarvestDateBetween(UUID harvesterId, LocalDateTime dayStart, LocalDateTime dayEnd);
+
+    // Harvester view
     List<Harvest> findAllByHarvesterIdAndHarvestDateBetween(UUID harvesterId, LocalDateTime dayStart, LocalDateTime dayEnd);
+
+    // Foreman view
     List<Harvest> findAllByForemanIdAndHarvesterNameContainingIgnoreCaseAndHarvestDateBetween(UUID foremanId, String harvesterName, LocalDateTime dayStart, LocalDateTime dayEnd);
+    List<Harvest> findAllByForemanIdAndHarvesterNameContainingIgnoreCase(UUID foremanId, String name);
+    List<Harvest> findAllByForemanIdAndHarvestDateBetween(UUID foremanId, LocalDateTime start, LocalDateTime end);
+    List<Harvest> findAllByForemanId(UUID foremanId);
+
 }
